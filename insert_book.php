a5a46957c1e2aabebdefbc4d6779e024a0ad2801
Lauri <lauri.nomtak@mail.ee>
2015-10-28 16:55:08 +0200

    HTML tabelite koostamine [Delivers #106848290]

---
+++ insert_book.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Uue kliendi lisamine</title>
+</head>
+<body>
+
+<form action="" method="POST" >
+
+    <table>
+        <tr>
+            <td>Pealkiri: </td>
+            <td><input type="text" name="title"></td>
+        </tr>
+        <tr>
+            <td>Autor: </td>
+            <td><input type="text" name="author"></td>
+        </tr>
+        <tr>
+            <td>Tüüp: </td>
+            <td><input type="text" name="type"></td>
+        </tr>
+        <tr>
+            <td>Kogus: </td>
+            <td><input type="text" name="quantity"></td>
+        </tr>
+        <tr>
+            <td>Tavatelefon: </td>
+            <td><input type="text" name="tel_nr"></td>
+        </tr>
+        <tr>
+            <td>Email: </td>
+            <td><input type="text" name="email"></td>
+        </tr>
+        <tr>
+            <td><input type="submit"> </td>
+        </tr>
+
+
+    </table>
+</form>
+
+</body>
+</html>
+
+
+<?php
+
---
+++ insert_client.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Uue kliendi lisamine</title>
+</head>
+<body>
+
+<form action="" method="POST" >
+
+    <table>
+        <tr>
+            <td>Eesnimi: </td>
+            <td><input type="text" name="fname"></td>
+        </tr>
+        <tr>
+            <td>Perekonnanimi: </td>
+            <td><input type="text" name="lname"></td>
+        </tr>
+        <tr>
+            <td>Isikukood: </td>
+            <td><input type="text" name="isikukood"></td>
+        </tr>
+        <tr>
+            <td>Mobiil: </td>
+            <td><input type="text" name="mobiil_tel"></td>
+        </tr>
+        <tr>
+            <td>Tavatelefon: </td>
+            <td><input type="text" name="tel_nr"></td>
+        </tr>
+        <tr>
+            <td>Email: </td>
+            <td><input type="text" name="email"></td>
+        </tr>
+        <tr>
+            <td><input type="submit"> </td>
+        </tr>
+
+
+    </table>
+</form>
+
+</body>
+</html>
+
+
+<?php
+
