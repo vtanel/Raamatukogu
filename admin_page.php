ec9cc645e585d110ae1d1201baa6992eea9c5166
rasmusreimaa <rasmus.reimaa@khk.ee>
2015-11-05 09:27:11 +0200

    Valmis teha sisselogitud admin page [Delivers #107463888]

---
+++ admin_page.php
@@ -0,0 +1,48 @@
+<!doctype html>
+<html lang="et">
+<head>
+    <meta charset="UTF-8">
+    <style>
+        .navbar, ul, li {
+            list-style-type: none;
+            display: inline;
+            float: left;
+        }
+
+        .navnupp {
+            width: 120px;
+            height: 40px;
+            border: 1px solid;
+            float: left;
+            display: inline-block;
+        }
+
+        .log_out {
+            float: right;
+        }
+    </style>
+</head>
+<body>
+<h1>Tere " admin #1 "</h1>
+
+<div class="navbar">
+    <ul>
+        <li><a href="#">
+                <div class="navnupp"><span>Kasutaja registeerimine</span></div>
+            </a></li>
+        <li><a href="#">
+                <div class="navnupp"><span>Sisesta raamat</span></div>
+            </a></li>
+        <li><a href="#">
+                <div class="navnupp"><span>Otsi kasutajat</span></div>
+            </a></li>
+        <li><a href="#">
+                <div class="navnupp"><span>Otsi raamatut</span></div>
+            </a></li>
+    </ul>
+</div>
+<a href="#">
+    <div class="log_out">LOG OUT</div>
+</a>
+</body>
+</html>
