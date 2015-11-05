340a7408302452277a7c8a984ee23abc19293eb6
Rasmus Pind <rasmus.pind@khk.ee>
2015-11-05 10:18:23 +0200

    Otsi kasutajat page [Delivers #107464118]

---
+++ search_user.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html lang="et">
+<head>
+    <meta charset="UTF-8">
+
+    <style>
+
+        .search_user {
+            border: 1px solid;
+            width: 390px;
+            height: 60px;
+        }
+
+    </style>
+
+</head>
+
+<body>
+
+<div class="search_user">
+
+    <form method="post">
+        <input type="search" name="search"/>
+        <input type="submit" name="submit" value="Esita"/>
+    </form>
+
+    <table border="1px">
+
+        <thead>
+        <td>ID</td>
+        <td>EESNIMI</td>
+        <td>PEREKONNANIMI</td>
+        <td>HETKEL LAENUS</td>
+        </thead>
+
+    </table>
+
+</div>
+
+</body>
+</html>
