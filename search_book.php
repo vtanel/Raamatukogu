0e33a52a857cb679d9a7adaf3a885e3118ddd697
rasmusreimaa <rasmus.reimaa@khk.ee>
2015-11-05 09:48:25 +0200

    Valmis ehitada raamatu  otsingu lehekülg [Delivers #107465556]

---
+++ search_book.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="et">
+<head>
+    <meta charset="UTF-8">
+    <style>
+        .search_book {
+            border: 1px solid;
+            width: 280px;
+            height: 60px;
+        }
+    </style>
+</head>
+<body>
+<div class="search_book">
+    <form method="post">
+        <input type="search" name="search"/>
+        <input type="submit" name="submit" value="Esita"/>
+    </form>
+    <table border="1px">
+        <thead>
+        <td>ID</td>
+        <td>PEALKIRI</td>
+        <td>AUTOR</td>
+        <td>ZANR</td>
+        <td>KOGUS</td>
+        </thead>
+    </table>
+</div>
+</body>
+</html>
