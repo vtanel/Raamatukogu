3fb7d107150359316691a5b0cfb7793b52a5ac9f
rasmusreimaa <rasmus.reimaa@khk.ee>
2015-11-10 14:27:35 +0200

    Admini logimine [Delivers #107800206]

--- login.php
+++ login.php
@@ -1,3 +1,19 @@
+<?php
+session_start();
+if(isset($_POST['submit'])){
+    require "connect.php";
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $result = mysqli_query($con, "SELECT * FROM `admins` WHERE username='$username' AND password='$password'");
+    if (mysqli_num_rows($result)==1){
+        $_SESSION['username'] = $username;
+        header("Location: admin_page.php");
+    }
+    else
+        echo "Kasutajanimi või parool on vale.";
+}
+?>
+
 <!doctype html>
 <html lang="en">
 <head>
@@ -20,31 +36,32 @@
 </head>
 <body>
 
-<div id='login' action='login.php' method='post' accept-charset='UTF-8'>
+<div id='login'>
+    <form method="post" accept-charset='UTF-8'>
 
-    <table>
+        <table>
 
-        <tr>
-            <td>
-                <input placeholder='Kasutajanimi' type='text' name='username' id='username' maxlength="15"/>
-            </td>
-        </tr>
+            <tr>
+                <td>
+                    <input placeholder='Kasutajanimi' type='text' name='username' id='username' maxlength="15"/>
+                </td>
+            </tr>
 
-        <tr>
-            <td>
-                <input placeholder='Parool' type='password' name='password' id='password' maxlength="15"/>
-                <br>
-            </td>
-        </tr>
+            <tr>
+                <td>
+                    <input placeholder='Parool' type='password' name='password' id='password' maxlength="15"/>
+                    <br>
+                </td>
+            </tr>
 
-        <tr>
-            <td>
-                <input type='submit' name='Submit' value='Logi sisse'/>
-            </td>
-        </tr>
+            <tr>
+                <td>
+                    <input type='submit' name='submit' value='Logi sisse'/>
+                </td>
+            </tr>
 
-    </table>
-
+        </table>
+    </form>
 </div>
 
 </body>
