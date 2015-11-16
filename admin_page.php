27fdc6811e49c174fc7aff3832236780fa27a285
rasmusreimaa <rasmus.reimaa@khk.ee>
2015-11-16 15:43:36 +0200

    Admin pagel vajutades " Kasutaja registreerimine " või mistahes lingi peale, avab samal lehel selle tegevuse, millele klikiti. [Delivers #107893556]

--- admin_page.php
+++ admin_page.php
@@ -11,9 +11,14 @@ require 'controllers/admins.php';
     <title>Raamatukogu</title>
     <meta charset="UTF-8">
     <style>
+        .search_user {
+            border: 1px solid;
+            width: 390px;
+            height: 60px;
+        }
         .navbar, ul, li {
             list-style-type: none;
-            display: inline;
+            display: inline-block;
             float: left;
         }
 
@@ -29,7 +34,17 @@ require 'controllers/admins.php';
             float: right;
         }
     </style>
-
+    <script type="text/javascript">
+        <!--
+        function toggle_visibility(id) {
+            var e = document.getElementById(id);
+            if (e.style.display == 'block')
+                e.style.display = 'none';
+            else
+                e.style.display = 'block';
+        }
+        //-->
+    </script>
 </head>
 
 <body>
@@ -38,25 +53,25 @@ require 'controllers/admins.php';
 
 <div class="navbar">
     <ul>
-        <li><a href="insert_client.php">
+        <li><a href="#" onclick="toggle_visibility('insert_client');">
                 <div class="navnupp"><span>Kasutaja registeerimine</span></div>
             </a></li>
 
         <br>
 
-        <li><a href="insert_book.php">
+        <li><a href="#" onclick="toggle_visibility('insert_book');">
                 <div class="navnupp"><span>Sisesta raamat</span></div>
             </a></li>
 
         <br>
 
-        <li><a href="search_user.php">
+        <li><a href="#" onclick="toggle_visibility('search_user');">
                 <div class="navnupp"><span>Otsi kasutajat</span></div>
             </a></li>
 
         <br>
 
-        <li><a href="search_book.php">
+        <li><a href="#" onclick="toggle_visibility('search_book');">
                 <div class="navnupp"><span>Otsi raamatut</span></div>
             </a></li>
 
@@ -66,84 +81,119 @@ require 'controllers/admins.php';
 
 </div>
 
-<!--  Calendar -->
-<?php
-$day_num = date("j"); //If today is September 29, $day_num=29
-$month_num = date("m"); //If today is September 29, $month_num=9
-$year = date("Y"); //4-digit year
-$date_today = getdate(mktime(0, 0, 0, $month_num, 1, $year)); //Returns array of date info for 1st day of this month
-$month_name = $date_today["month"]; //Example: "September" - to label the Calendar
-$first_week_day = $date_today["wday"]; //"wday" is 0-6, 0 being Sunday. This is for day 1 of this month
-
-$cont = true;
-$today = 27; //The last day of the month must be >27, so start here
-while (($today <= 32) && ($cont)) //At 32, we have to be rolling over to the next month
-{
-//Iterate through, incrementing $today
-//Get the date information for the (hypothetical) date $month_num/$today/$year
-    $date_today = getdate(mktime(0, 0, 0, $month_num, $today, $year));
-//Once $date_today's month ($date_today["mon"]) rolls over to the next month, we've found the $lastday
-    if ($date_today["mon"] != $month_num) {
-        $lastday = $today - 1; //If we just rolled over to the next month, need to subtract 1 to get our $lastday
-        $cont = false; //This kicks us out of the while loop
-    }
-    $today++;
-}
-?>
-
-<table cellspacing=0 cellpadding=5 frame='all' rules='all' style='border:#808080 1px solid;'>
-    <caption> <?php echo $month_name ?></caption>
-    <tr align=left>
-        <th>P</th>
-        <th>E</th>
-        <th>T</th>
-        <th>K</th>
-        <th>N</th>
-        <th>R</th>
-        <th>L</th>
-    </tr>
-
-    <?php
-    $day = 1; //This variable will track the day of the month
-    $wday = $first_week_day; //This variable will track the day of the week (0-6, with Sunday being 0)
-    $firstweek = true; //Initialize $firstweek variable so we can deal with it first
-    while ($day <= $lastday) //Iterate through all days of the month
-    {
-        if ($firstweek) //Special case - first week (remember we initialized $first_week_day above)
-        {
-            echo "<tr align=left>";
-            for ($i = 1; $i <= $first_week_day; $i++) {
-                echo "<td> </td>"; //Put a blank cell for each day until you hit $first_week_day
-            }
-            $firstweek = false; //Great, we're done with the blank cells
-        }
-        if ($wday == 0) //Start a new row every Sunday
-            echo "<tr align=left>";
-
-        echo "<td";
-        if ($day == $day_num) echo " bgcolor='yellow'"; //highlight TODAY in yellow
-        echo ">$day</td>";
-        if ($wday == 6)
-            echo "</tr>"; //If today is Saturday, close this row
-
-        $wday++; //Increment $wday
-        $wday = $wday % 7; //Make sure $wday stays between 0 and 6 (so when $wday++ == 7, this will take it back to 0)
-        $day++; //Increment $day
-    }
-
-    while ($wday <= 6) //Until we get through Saturday
-    {
-        echo "<td> </td>"; //Output an empty cell
-        $wday++;
-    }
-    echo "</tr></table>";
-    ?>
-
-    <!-- Calendar end -->
-
     <a href="login.php">
         <div class="log_out">Logi välja</div>
     </a>
 
+<div id="insert_client">
+    <h1><b>Laenutaja registreerimine</b></h1>
+
+    <form action="" method="POST">
+
+        <table>
+            <tr>
+                <td>Eesnimi:</td>
+                <td><input type="text" max="" name="fname"></td>
+            </tr>
+            <tr>
+                <td>Perekonnanimi:</td>
+                <td><input type="text" name="lname"></td>
+            </tr>
+            <tr>
+                <td>Isikukood:</td>
+                <td><input type="number" name="p_code"></td>
+            </tr>
+            <tr>
+                <td>Mobiil:</td>
+                <td><input type="number" name="mobile"></td>
+            </tr>
+            <tr>
+                <td>Email:</td>
+                <td><input type="text" name="e_mail"></td>
+            </tr>
+            <tr>
+                <td><input type="submit" value="Salvesta"></td>
+            </tr>
+
+
+        </table>
+    </form>
+</div>
+<div id="insert_book">
+
+
+    <h1><b>Raamatu sisestamine</b></h1>
+
+    <form action="" method="POST">
+
+        <table>
+            <tr>
+                <td>Pealkiri:</td>
+                <td><input type="text" name="title"></td>
+            </tr>
+            <tr>
+                <td>Autor:</td>
+                <td><input type="text" name="author"></td>
+            </tr>
+            <tr>
+                <td>Tüüp:</td>
+                <td><input type="text" name="type"></td>
+            </tr>
+            <tr>
+                <td>Kogus:</td>
+                <td><input type="number" name="quantity"></td>
+            </tr>
+            <tr>
+                <td><input type="submit" name="Salvesta"></td>
+            </tr>
+        </table>
+    </form>
+</div>
+<div id="search_user">
+
+
+    <h1><b>Kasutajad</b></h1>
+
+    <div class="search_user">
+
+        <form method="post">
+            <input type="search" name="search" placeholder="Otsi kasutajat..."/>
+            <input type="submit" name="submit" value="Otsi"/>
+        </form>
+
+        <table border="1px">
+
+            <thead>
+            <td>ID</td>
+            <td>EESNIMI</td>
+            <td>PEREKONNANIMI</td>
+            <td>HETKEL LAENUS</td>
+            </thead>
+
+        </table>
+
+    </div>
+</div>
+<div id="search_book">
+
+
+    <h1><b>Raamatud</b></h1>
+
+    <div class="search_book">
+        <form method="post">
+            <input type="search" name="search" placeholder="Otsi raamatut..."/>
+            <input type="submit" name="submit" value="Otsi"/>
+        </form>
+        <table border="1px">
+            <thead>
+            <td>ID</td>
+            <td>PEALKIRI</td>
+            <td>AUTOR</td>
+            <td>ZANR</td>
+            <td>KOGUS</td>
+            </thead>
+        </table>
+    </div>
+</div>
 </body>
 </html>
