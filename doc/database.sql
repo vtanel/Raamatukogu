06234ec6a58b245dd6922a94a558bc5d2d2be710
Tanel Viira <tanel.viira@khk.ee>
2015-11-18 09:34:01 +0200

    Added database.sql

--- doc/database.sql
+++ doc/database.sql
@@ -1,36 +1,35 @@
--- MySQL dump 10.13  Distrib 5.6.26, for Win32 (x86)
+-- MySQL dump 10.13  Distrib 5.6.16, for Win32 (x86)
 --
 -- Host: localhost    Database: raamatukogu
 -- ------------------------------------------------------
--- Server version	5.6.26
+-- Server version	5.6.16
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT = @@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS = @@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION = @@COLLATION_CONNECTION */;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE = @@TIME_ZONE */;
-/*!40103 SET TIME_ZONE = '+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS = @@UNIQUE_CHECKS, UNIQUE_CHECKS = 0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS = @@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS = 0 */;
-/*!40101 SET @OLD_SQL_MODE = @@SQL_MODE, SQL_MODE = 'NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES = @@SQL_NOTES, SQL_NOTES = 0 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
 -- Table structure for table `admins`
 --
 
 DROP TABLE IF EXISTS `admins`;
-/*!40101 SET @saved_cs_client = @@character_set_client */;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `admins` (
-  `admin_ID`       INT(11) NOT NULL AUTO_INCREMENT,
-  `admin_username` VARCHAR(255)     DEFAULT NULL,
-  `admin_password` VARCHAR(255)     DEFAULT NULL,
+  `admin_ID` int(11) NOT NULL AUTO_INCREMENT,
+  `admin_username` varchar(255) DEFAULT NULL,
+  `admin_password` varchar(255) DEFAULT NULL,
+  `admin_fname` varchar(255) NOT NULL,
+  `admin_lname` varchar(255) NOT NULL,
   PRIMARY KEY (`admin_ID`)
-)
-  ENGINE = InnoDB
-  AUTO_INCREMENT = 2
-  DEFAULT CHARSET = latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -39,7 +38,7 @@ CREATE TABLE `admins` (
 
 LOCK TABLES `admins` WRITE;
 /*!40000 ALTER TABLE `admins` DISABLE KEYS */;
-INSERT INTO `admins` VALUES (0, 'admin', 'admin'), (1, 'test', 'test');
+INSERT INTO `admins` VALUES (0,'admin','admin','pROOV','Proov'),(1,'test','test','TEST','TESTING');
 /*!40000 ALTER TABLE `admins` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -48,16 +47,14 @@ UNLOCK TABLES;
 --
 
 DROP TABLE IF EXISTS `blacklist`;
-/*!40101 SET @saved_cs_client = @@character_set_client */;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `blacklist` (
-  `fname`     VARCHAR(255) NOT NULL,
-  `lname`     VARCHAR(255) NOT NULL,
-  `client_ID` INT(11)      NOT NULL AUTO_INCREMENT,
+  `fname` varchar(255) NOT NULL,
+  `lname` varchar(255) NOT NULL,
+  `client_ID` int(11) NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (`client_ID`)
-)
-  ENGINE = InnoDB
-  DEFAULT CHARSET = latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -74,19 +71,16 @@ UNLOCK TABLES;
 --
 
 DROP TABLE IF EXISTS `books`;
-/*!40101 SET @saved_cs_client = @@character_set_client */;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `books` (
-  `book_ID`       INT(11)      NOT NULL AUTO_INCREMENT,
-  `book_title`    VARCHAR(255) NOT NULL,
-  `book_author`   VARCHAR(255) NOT NULL,
-  `book_type`     VARCHAR(255) NOT NULL,
-  `book_quantity` INT(11)      NOT NULL,
+  `book_ID` int(11) NOT NULL AUTO_INCREMENT,
+  `book_title` varchar(255) NOT NULL,
+  `book_author` varchar(255) NOT NULL,
+  `book_type` varchar(255) NOT NULL,
+  `book_quantity` int(11) NOT NULL,
   PRIMARY KEY (`book_ID`)
-)
-  ENGINE = InnoDB
-  AUTO_INCREMENT = 61
-  DEFAULT CHARSET = latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=65 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -95,7 +89,7 @@ CREATE TABLE `books` (
 
 LOCK TABLES `books` WRITE;
 /*!40000 ALTER TABLE `books` DISABLE KEYS */;
-INSERT INTO `books` VALUES (60, 'Kevade', 'Luts', 'Draama', 3);
+INSERT INTO `books` VALUES (60,'Kevade','Luts','Draama',3),(61,'wqdqwed','dsadsa','dasdsa',21),(62,'wqdqwed','dsadsa','dasdsa',21),(63,'wqdqwed','dsadsa','dasdsa',21),(64,'wqdqwed','dsadsa','dasdsa',21);
 /*!40000 ALTER TABLE `books` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -104,22 +98,19 @@ UNLOCK TABLES;
 --
 
 DROP TABLE IF EXISTS `client`;
-/*!40101 SET @saved_cs_client = @@character_set_client */;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `client` (
-  `fname`      VARCHAR(255) NOT NULL,
-  `lname`      VARCHAR(255) NOT NULL,
-  `p_code`     INT(11)      NOT NULL,
-  `mobile`     INT(11)      NOT NULL,
-  `mail`       VARCHAR(255) NOT NULL,
-  `blacklist`  TINYINT(1)   NOT NULL,
-  `total_rent` INT(11)      NOT NULL,
-  `client_ID`  INT(11)      NOT NULL AUTO_INCREMENT,
+  `fname` varchar(255) NOT NULL,
+  `lname` varchar(255) NOT NULL,
+  `p_code` int(11) NOT NULL,
+  `mobile` int(11) NOT NULL,
+  `mail` varchar(255) NOT NULL,
+  `blacklist` tinyint(1) NOT NULL,
+  `total_rent` int(11) NOT NULL,
+  `client_ID` int(11) NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (`client_ID`)
-)
-  ENGINE = InnoDB
-  AUTO_INCREMENT = 5
-  DEFAULT CHARSET = latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -128,19 +119,17 @@ CREATE TABLE `client` (
 
 LOCK TABLES `client` WRITE;
 /*!40000 ALTER TABLE `client` DISABLE KEYS */;
-INSERT INTO `client`
-VALUES ('asasd', 'asdsad', 2147483647, 123, 'asd', 0, 1, 1), ('asd', 'asd', 2147483647, 1212, 'asd', 0, 1, 2),
-  ('asd', 'asd', 2147483647, 1212, 'asd', 0, 1, 3), ('asd', 'asd', 2147483647, 123, 'asd', 0, 1, 4);
+INSERT INTO `client` VALUES ('asasd','asdsad',2147483647,123,'asd',0,1,1),('asd','asd',2147483647,1212,'asd',0,1,2),('asd','asd',2147483647,1212,'asd',0,1,3),('asd','asd',2147483647,123,'asd',0,1,4);
 /*!40000 ALTER TABLE `client` ENABLE KEYS */;
 UNLOCK TABLES;
-/*!40103 SET TIME_ZONE = @OLD_TIME_ZONE */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-/*!40101 SET SQL_MODE = @OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS = @OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS = @OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT = @OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS = @OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION = @OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES = @OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2015-11-11  9:25:55
+-- Dump completed on 2015-11-18  9:33:02
