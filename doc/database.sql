39465fdc5732352d49f7db48f18e04e6409c06aa
rasmusreimaa <rasmus.reimaa@khk.ee>
2015-11-23 14:22:26 +0200

    Lisatud adminid andmebaasi [Delivers #108417174]

--- doc/database.sql
+++ doc/database.sql
@@ -1,107 +1,102 @@
--- MySQL dump 10.13  Distrib 5.6.16, for Win32 (x86)
+-- phpMyAdmin SQL Dump
+-- version 4.4.14
+-- http://www.phpmyadmin.net
 --
--- Host: localhost    Database: raamatukogu
--- ------------------------------------------------------
--- Server version	5.6.16
+-- Host: 127.0.0.1
+-- Loomise aeg: Nov 23, 2015 kell 01:21 p.k.
+-- Serveri versioon: 5.6.26
+-- PHP versioon: 5.6.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
--- Table structure for table `admins`
+-- Andmebaas: `raamatukogu`
 --
 
-DROP TABLE IF EXISTS `admins`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `admins` (
-  `admin_ID` int(11) NOT NULL AUTO_INCREMENT,
+-- --------------------------------------------------------
+
+--
+-- Tabeli struktuur tabelile `admins`
+--
+
+CREATE TABLE IF NOT EXISTS `admins` (
+  `admin_ID`    INT(11) NOT NULL,
   `admin_username` varchar(255) DEFAULT NULL,
   `admin_password` varchar(255) DEFAULT NULL,
   `admin_fname` varchar(255) NOT NULL,
   `admin_lname` varchar(255) NOT NULL,
-  `admin_image` text NOT NULL,
-  PRIMARY KEY (`admin_ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `admin_image` TEXT    NOT NULL
+)
+  ENGINE = InnoDB
+  AUTO_INCREMENT = 8
+  DEFAULT CHARSET = latin1;
 
 --
--- Dumping data for table `admins`
+-- Andmete tõmmistamine tabelile `admins`
 --
 
-LOCK TABLES `admins` WRITE;
-/*!40000 ALTER TABLE `admins` DISABLE KEYS */;
-INSERT INTO `admins` VALUES (0,'admin','admin','pROOV','Proov','https://www.wonderplugin.com/wp-content/plugins/wonderplugin-lightbox/images/demo-image0.jpg'),(1,'test','test','TEST','TESTING','http://www.wpclipart.com/people/faces/men_faces/men_faces_04/face_man_long.png');
-/*!40000 ALTER TABLE `admins` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `admins` (`admin_ID`, `admin_username`, `admin_password`, `admin_fname`, `admin_lname`, `admin_image`)
+VALUES
+  (1, 'test', 'test', 'TEST', 'TESTING',
+   'http://www.wpclipart.com/people/faces/men_faces/men_faces_04/face_man_long.png'),
+  (2, 'admin', 'admin', 'Admin', 'Nimda', ''),
+  (3, 'alvar', 'pungar', 'Alvar', 'Pungar', ''),
+  (4, 'tanel', 'viira', 'Tanel', 'Viira', ''),
+  (5, 'rasmusr', 'reimaa', 'Rasmus', 'Reimaa', ''),
+  (6, 'rasmusp', 'pind', 'Rasmus', 'Pind', ''),
+  (7, 'lauri', 'nomtak', 'Lauri', 'Nõmtak', '');
+
+-- --------------------------------------------------------
 
 --
--- Table structure for table `blacklist`
+-- Tabeli struktuur tabelile `blacklist`
 --
 
-DROP TABLE IF EXISTS `blacklist`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `blacklist` (
+CREATE TABLE IF NOT EXISTS `blacklist` (
   `fname` varchar(255) NOT NULL,
   `lname` varchar(255) NOT NULL,
-  `client_ID` int(11) NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (`client_ID`)
+  `client_ID` INT(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+
+-- --------------------------------------------------------
 
 --
--- Dumping data for table `blacklist`
+-- Tabeli struktuur tabelile `books`
 --
 
-LOCK TABLES `blacklist` WRITE;
-/*!40000 ALTER TABLE `blacklist` DISABLE KEYS */;
-/*!40000 ALTER TABLE `blacklist` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `books`
---
-
-DROP TABLE IF EXISTS `books`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `books` (
-  `book_ID` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `books` (
+  `book_ID`       INT(11) NOT NULL,
   `book_title` varchar(255) NOT NULL,
   `book_author` varchar(255) NOT NULL,
   `book_type` varchar(255) NOT NULL,
-  `book_quantity` int(11) NOT NULL,
-  PRIMARY KEY (`book_ID`)
+  `book_quantity` INT(11) NOT NULL
 ) ENGINE=InnoDB AUTO_INCREMENT=65 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `books`
+-- Andmete tõmmistamine tabelile `books`
 --
 
-LOCK TABLES `books` WRITE;
-/*!40000 ALTER TABLE `books` DISABLE KEYS */;
-INSERT INTO `books` VALUES (60,'Kevade','Luts','Draama',3),(61,'wqdqwed','dsadsa','dasdsa',21),(62,'wqdqwed','dsadsa','dasdsa',21),(63,'wqdqwed','dsadsa','dasdsa',21),(64,'wqdqwed','dsadsa','dasdsa',21);
-/*!40000 ALTER TABLE `books` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `books` (`book_ID`, `book_title`, `book_author`, `book_type`, `book_quantity`) VALUES
+  (60, 'Kevade', 'Luts', 'Draama', 3),
+  (61, 'wqdqwed', 'dsadsa', 'dasdsa', 21),
+  (62, 'wqdqwed', 'dsadsa', 'dasdsa', 21),
+  (63, 'wqdqwed', 'dsadsa', 'dasdsa', 21),
+  (64, 'wqdqwed', 'dsadsa', 'dasdsa', 21);
+
+-- --------------------------------------------------------
 
 --
--- Table structure for table `client`
+-- Tabeli struktuur tabelile `client`
 --
 
-DROP TABLE IF EXISTS `client`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `client` (
+CREATE TABLE IF NOT EXISTS `client` (
   `fname` varchar(255) NOT NULL,
   `lname` varchar(255) NOT NULL,
   `p_code` int(11) NOT NULL,
@@ -109,28 +104,71 @@ CREATE TABLE `client` (
   `mail` varchar(255) NOT NULL,
   `blacklist` tinyint(1) NOT NULL,
   `total_rent` int(11) NOT NULL,
-  `client_ID` int(11) NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (`client_ID`)
+  `client_ID` INT(11) NOT NULL
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `client`
+-- Andmete tõmmistamine tabelile `client`
 --
 
-LOCK TABLES `client` WRITE;
-/*!40000 ALTER TABLE `client` DISABLE KEYS */;
-INSERT INTO `client` VALUES ('asasd','asdsad',2147483647,123,'asd',0,1,1),('asd','asd',2147483647,1212,'asd',0,1,2),('asd','asd',2147483647,1212,'asd',0,1,3),('asd','asd',2147483647,123,'asd',0,1,4);
-/*!40000 ALTER TABLE `client` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+INSERT INTO `client` (`fname`, `lname`, `p_code`, `mobile`, `mail`, `blacklist`, `total_rent`, `client_ID`) VALUES
+  ('asasd', 'asdsad', 2147483647, 123, 'asd', 0, 1, 1),
+  ('asd', 'asd', 2147483647, 1212, 'asd', 0, 1, 2),
+  ('asd', 'asd', 2147483647, 1212, 'asd', 0, 1, 3),
+  ('asd', 'asd', 2147483647, 123, 'asd', 0, 1, 4);
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+--
+-- Indeksid tõmmistatud tabelitele
+--
+
+--
+-- Indeksid tabelile `admins`
+--
+ALTER TABLE `admins`
+ADD PRIMARY KEY (`admin_ID`);
+
+--
+-- Indeksid tabelile `blacklist`
+--
+ALTER TABLE `blacklist`
+ADD PRIMARY KEY (`client_ID`);
+
+--
+-- Indeksid tabelile `books`
+--
+ALTER TABLE `books`
+ADD PRIMARY KEY (`book_ID`);
+
+--
+-- Indeksid tabelile `client`
+--
+ALTER TABLE `client`
+ADD PRIMARY KEY (`client_ID`);
+
+--
+-- AUTO_INCREMENT tõmmistatud tabelitele
+--
+
+--
+-- AUTO_INCREMENT tabelile `admins`
+--
+ALTER TABLE `admins`
+MODIFY `admin_ID` INT(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT = 8;
+--
+-- AUTO_INCREMENT tabelile `blacklist`
+--
+ALTER TABLE `blacklist`
+MODIFY `client_ID` INT(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT tabelile `books`
+--
+ALTER TABLE `books`
+MODIFY `book_ID` INT(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT = 65;
+--
+-- AUTO_INCREMENT tabelile `client`
+--
+ALTER TABLE `client`
+MODIFY `client_ID` INT(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT = 5;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2015-11-18  9:44:59
