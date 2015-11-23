a05b06b4dbc7a7d964d9f4a5fecc790a3a4b2b94
rasmusreimaa <rasmus.reimaa@khk.ee>
2015-11-23 15:17:04 +0200

    mysqldump

--- doc/database.sql
+++ doc/database.sql
@@ -4,6 +4,7 @@
 -- ------------------------------------------------------
 -- Server version	5.6.26
 
+
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
