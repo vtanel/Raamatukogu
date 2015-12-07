23f3e1928c5eccb128f88d65fd85efce5c266990
rasmusreimaa <rasmus.reimaa@khk.ee>
2015-12-07 13:43:56 +0200

    Saab raamatu andmeid muuta

--- controllers/book.php
+++ controllers/book.php
@@ -1,15 +1,33 @@
 <?php
 $book_id = isset($_GET['id']) ? $_GET['id'] : -1;
-$q = mysqli_query($con, "
-SELECT books.book_title, authors.author_name, genres.genre_name, books.book_quantiy
-FROM books
-JOIN authors ON books.author_id = authors.author_id
-JOIN genres ON books.genre_id = genres.genre_id
+$q = mysqli_query($con,
+    "SELECT books.book_title, authors.author_name, genres.genre_name, books.book_quantiy
+    FROM books
+    JOIN authors ON books.author_id = authors.author_id
+    JOIN genres ON books.genre_id = genres.genre_id
 
-WHERE books.book_id=$book_id
-
-
-
-
-") or die(mysqli_error($con));
+    WHERE books.book_id=$book_id")
+or die(mysqli_error($con));
 $raamat = mysqli_fetch_assoc($q);
+
+
+if (isset($_POST['changebook'])) {
+    $author_name = $_POST['author_name'];
+    $genre = $_POST['genre'];
+    $book_title = $_POST['book_title'];
+    $book_quantiy = $_POST['book_quantiy'];
+
+    $q = mysqli_query($con,
+        "UPDATE books, authors, genres
+        SET book_title = '$book_title',
+        book_quantiy = '$book_quantiy',
+        authors.author_name = '$author_name',
+        genres.genre_name = '$genre'
+        WHERE authors.author_id=books.author_id AND genres.genre_id=books.genre_id AND book_id = '$book_id' ");
+
+    if ($q) {
+        echo "Andmed muudetud";
+    } else {
+        echo "Andmete muutmisel tekkis viga";
+    }
+}
--- views/book.php
+++ views/book.php
@@ -1,50 +1,28 @@
 <?php require 'controllers/book.php'; ?>
 
+<form method="post">
 <table class="search-table">
     <tbody>
     <td colspan="2">Raamatu andmed</td>
     <tr>
         <td>Autor</td>
-        <td><?= $raamat['author_name'] ?></td>
+        <td><input type="text" name="author_name" value="<?= $raamat['author_name'] ?>"/></td>
     </tr>
     <tr>
         <td>Zanr</td>
-        <td><?= $raamat['genre_name'] ?></td>
+        <td><input type="text" name="genre" value="<?= $raamat['genre_name'] ?>"/></td>
     </tr>
     <tr>
         <td>Pealkiri</td>
-        <td><?= $raamat['book_title'] ?></td>
+        <td><input type="text" name="book_title" value="<?= $raamat['book_title'] ?>"/></td>
     </tr>
     <tr>
         <td>Kogus</td>
-        <td><?= $raamat['book_quantiy'] ?></td>
+        <td><input type="text" name="book_quantiy" value="<?= $raamat['book_quantiy'] ?>"/></td>
+    </tr>
+    <tr>
+        <td><input type="submit" value="Muuda andmeid" name="changebook"/></td>
     </tr>
     </tbody>
 </table>
-
-<form action="" method="POST">
-    <table class="search-table">
-        <tbody>
-        <td colspan="2">Muuda andmeid</td>
-        <tr>
-            <td>Pealkiri:</td>
-            <td><input type="text" name="title"></td>
-        </tr>
-        <tr>
-            <td>Autor:</td>
-            <td><input type="text" name="author"></td>
-        </tr>
-        <tr>
-            <td>Tüüp:</td>
-            <td><input type="text" name="genre"></td>
-        </tr>
-        <tr>
-            <td>Kogus:</td>
-            <td><input type="number" name="quantity"></td>
-        </tr>
-        <tr>
-            <td><input type="submit" value="Muuda andmeid" name="changebook"></td>
-        </tr>
-        </tbody>
-    </table>
 </form>
