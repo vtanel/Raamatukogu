65b44d5582f45c26079d5024b5f5486de7f87fc9
Lauri <lauri.nomtak@mail.ee>
2015-12-14 09:11:31 +0200

    Raamatu andmete muutmisel kuvatakse kohe uued andmed

--- controllers/book.php
+++ controllers/book.php
@@ -27,7 +27,17 @@ if (isset($_POST['changebook'])) {
 
     if ($q) {
         echo "Andmed muudetud";
+        $q = mysqli_query($con,
+            "SELECT books.book_title, authors.author_name, genres.genre_name, books.book_quantiy
+    FROM books
+    JOIN authors ON books.author_id = authors.author_id
+    JOIN genres ON books.genre_id = genres.genre_id
+
+    WHERE books.book_id=$book_id")
+        or die(mysqli_error($con));
+        $raamat = mysqli_fetch_assoc($q);
     } else {
         echo "Andmete muutmisel tekkis viga";
     }
+
 }
