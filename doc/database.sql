b8524c900f12b6c500b1ed9936b788d32bce1d8c
rasmusreimaa <rasmus.reimaa@khk.ee>
2016-01-11 14:59:43 +0200

    mysqldump

--- doc/database.sql
+++ doc/database.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.6.26, for Win32 (x86)
+-- MySQL dump 10.16  Distrib 10.1.9-MariaDB, for Win32 (AMD64)
 --
 -- Host: localhost    Database: raamatukogu
 -- ------------------------------------------------------
--- Server version	5.6.26
+-- Server version	10.1.9-MariaDB
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -39,7 +39,12 @@ CREATE TABLE `admins` (
 
 LOCK TABLES `admins` WRITE;
 /*!40000 ALTER TABLE `admins` DISABLE KEYS */;
-INSERT INTO `admins` VALUES (1,'test','test','TEST','TESTING','http://www.wpclipart.com/people/faces/men_faces/men_faces_04/face_man_long.png'),(2,'admin','d033e22ae348aeb5660fc2140aec35850c4da997','Admin','Nimda',''),(3,'alvar','pungar','Alvar','Pungar',''),(4,'tanel','viira','Tanel','Viira',''),(5,'rasmusr','reimaa','Rasmus','Reimaa',''),(6,'rasmusp','pind','Rasmus','Pind',''),(7,'lauri','nomtak','Lauri','Nõmtak','');
+INSERT INTO `admins` VALUES
+  (1, 'test', 'test', 'Test', 'Test', 'http://www.wpclipart.com/people/faces/men_faces/men_faces_04/face_man_long.png'),
+  (2, 'admin', 'd033e22ae348aeb5660fc2140aec35850c4da997', 'Admin', 'Nimda', ''),
+  (3, 'alvar', 'pungar', 'Alvar', 'Pungar', ''), (4, 'tanel', 'viira', 'Tanel', 'Viira', ''),
+  (5, 'rasmusr', 'reimaa', 'Rasmus', 'Reimaa', ''), (6, 'rasmusp', 'pind', 'Rasmus', 'Pind', ''),
+  (7, 'lauri', 'nomtak', 'Lauri', 'Nõmtak', '');
 /*!40000 ALTER TABLE `admins` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -56,7 +61,7 @@ CREATE TABLE `authors` (
   PRIMARY KEY (`author_id`)
 )
   ENGINE = InnoDB
-  AUTO_INCREMENT = 13
+  AUTO_INCREMENT = 2
   DEFAULT CHARSET = latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -66,9 +71,7 @@ CREATE TABLE `authors` (
 
 LOCK TABLES `authors` WRITE;
 /*!40000 ALTER TABLE `authors` DISABLE KEYS */;
-INSERT INTO `authors`
-VALUES (1, 'Oskar Luts'), (2, 'Anton H. Tammsaare'), (3, 'Autor1'), (4, 'luts'), (5, 'asd'), (11, 'Sipsik'),
-  (12, 'auto 7');
+INSERT INTO `authors` VALUES (1, 'Oskar Luts');
 /*!40000 ALTER TABLE `authors` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -88,7 +91,7 @@ CREATE TABLE `books` (
   PRIMARY KEY (`book_id`)
 )
   ENGINE = InnoDB
-  AUTO_INCREMENT = 33
+  AUTO_INCREMENT = 3
   DEFAULT CHARSET = latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -98,10 +101,7 @@ CREATE TABLE `books` (
 
 LOCK TABLES `books` WRITE;
 /*!40000 ALTER TABLE `books` DISABLE KEYS */;
-INSERT INTO `books`
-VALUES (1, 1, 1, 'Kevade', '28'), (2, 2, 2, 'Tõde ja õigus', '5'), (3, 3, 3, 'Raamat1', ''), (17, 5, 4, 'Raamat2', ''),
-  (28, 2, 1, 'Tõde ja õigus II', '6'), (29, 1, 1, 'Suvi ', '3'), (30, 11, NULL, 'Klaabu', '7'),
-  (31, 11, NULL, 'asd', '9'), (32, 12, 12, 'raama3 ', '7');
+INSERT INTO `books` VALUES (1, 1, 1, 'Kevade', '9'), (2, 1, 1, 'asd', '9');
 /*!40000 ALTER TABLE `books` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -122,7 +122,7 @@ CREATE TABLE `clients` (
   PRIMARY KEY (`client_id`)
 )
   ENGINE = InnoDB
-  AUTO_INCREMENT = 5
+  AUTO_INCREMENT = 4
   DEFAULT CHARSET = latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -132,9 +132,8 @@ CREATE TABLE `clients` (
 
 LOCK TABLES `clients` WRITE;
 /*!40000 ALTER TABLE `clients` DISABLE KEYS */;
-INSERT INTO `clients` VALUES (1, 'Piilu', 'Part', '59912158547', '5555555', 'lauri.nomtak@khk.ee'),
-  (2, 'Jaana', 'Lind', '', '5454546', 'jaana@lind.ee'), (3, 'Jaana', 'Lind', '49802306985', '5454546', 'jaana@lind.ee'),
-  (4, 'Lauri', 'Nõmtak', '12345678910', '97', 'laurinomtak@gmail.com');
+INSERT INTO `clients` VALUES (1, 'Piilu', 'Part', '59912158547', '5555555', 'piilu@part.ee'),
+  (2, 'Jaana', 'Lind', '', '5454546', 'jaana@lind.ee'), (3, 'Jaana', 'Lind', '49802306985', '5454546', 'jaana@lind.ee');
 /*!40000 ALTER TABLE `clients` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -151,7 +150,7 @@ CREATE TABLE `genres` (
   PRIMARY KEY (`genre_id`)
 )
   ENGINE = InnoDB
-  AUTO_INCREMENT = 13
+  AUTO_INCREMENT = 2
   DEFAULT CHARSET = latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -161,37 +160,8 @@ CREATE TABLE `genres` (
 
 LOCK TABLES `genres` WRITE;
 /*!40000 ALTER TABLE `genres` DISABLE KEYS */;
-INSERT INTO `genres`
-VALUES (1, 'Draama'), (2, 'Komöödia'), (3, 'Tüüp1'), (4, 'asd'), (10, 'Laste'), (11, 'type'), (12, 'tere');
+INSERT INTO `genres` VALUES (1, 'Draama');
 /*!40000 ALTER TABLE `genres` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `reminders`
---
-
-DROP TABLE IF EXISTS `reminders`;
-/*!40101 SET @saved_cs_client = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `reminders` (
-  `reminder_id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `send_date`   DATETIME                  DEFAULT NULL,
-  `rent_id`     INT(10) UNSIGNED          DEFAULT NULL,
-  PRIMARY KEY (`reminder_id`),
-  KEY `rent_id` (`rent_id`),
-  CONSTRAINT `reminders_ibfk_1` FOREIGN KEY (`rent_id`) REFERENCES `rent` (`rent_id`)
-)
-  ENGINE = InnoDB
-  DEFAULT CHARSET = latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `reminders`
---
-
-LOCK TABLES `reminders` WRITE;
-/*!40000 ALTER TABLE `reminders` DISABLE KEYS */;
-/*!40000 ALTER TABLE `reminders` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -202,14 +172,17 @@ DROP TABLE IF EXISTS `rent`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `rent` (
-  `rent_id`         int(10) unsigned NOT NULL DEFAULT '0',
-  `client_id`       int(11) unsigned          DEFAULT NULL,
-  `book_id`         int(11) unsigned          DEFAULT NULL,
-  `rent_start_date` datetime                  DEFAULT NULL,
-  `rent_end_date`   datetime                  DEFAULT NULL,
-  `last_reminder`   DATETIME                  DEFAULT CURRENT_TIMESTAMP,
+  `rent_id`          INT(11) NOT NULL AUTO_INCREMENT,
+  `client_id`        INT(11) UNSIGNED DEFAULT NULL,
+  `book_id`          INT(11) UNSIGNED DEFAULT NULL,
+  `rent_start_date`  DATETIME         DEFAULT NULL,
+  `rent_end_date`    DATETIME         DEFAULT NULL,
+  `rent_return_date` DATETIME         DEFAULT NULL,
   PRIMARY KEY (`rent_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+)
+  ENGINE = InnoDB
+  AUTO_INCREMENT = 4
+  DEFAULT CHARSET = latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -218,13 +191,8 @@ CREATE TABLE `rent` (
 
 LOCK TABLES `rent` WRITE;
 /*!40000 ALTER TABLE `rent` DISABLE KEYS */;
-INSERT INTO `rent` VALUES (0, 2, 1, '2015-11-02 14:17:50', '2015-12-13 14:17:57', '2016-12-15 15:35:08'),
-  (1, 1, 1, '2015-11-28 06:00:00', '2015-11-30 06:00:00', '2016-12-15 15:35:20'),
-  (2, 4, 1, '2015-11-11 00:00:00', '2015-12-26 00:00:00', '2016-12-15 15:35:22'),
-  (3, 4, 2, '2015-11-02 15:09:39', '2015-12-01 15:09:43', '2016-12-15 15:35:24'),
-  (4, 1, 1, '2015-12-15 15:38:10', '2014-12-15 15:38:13', '2016-01-04 08:52:56'),
-  (5, 1, 1, '2015-12-15 15:40:51', '2014-12-15 15:40:53', '2016-01-04 08:49:27'),
-  (6, 1, 1, '2016-01-04 08:55:24', '2016-01-04 08:55:27', '2016-01-04 08:55:30');
+INSERT INTO `rent` VALUES (1, 1, 2, NULL, NULL, NULL), (2, 1, 2, '2016-01-04 09:53:25', '2016-01-05 09:53:25', NULL),
+  (3, 1, 1, '2016-01-11 14:05:40', '2016-01-25 14:05:40', NULL);
 /*!40000 ALTER TABLE `rent` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -237,4 +205,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-01-04  9:12:20
+-- Dump completed on 2016-01-11 14:58:38
