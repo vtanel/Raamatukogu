8e84e03096b4b1ffb104744c9ba47f59dbc9a356
Lauri <lauri.nomtak@mail.ee>
2016-01-18 15:28:53 +0200

    Raamatute otsimine on muudetud asünkroonseks.

--- controllers/search_book.php
+++ controllers/search_book.php
@@ -1,12 +1,24 @@
 <?php
 
+if (empty($_POST['search'])) {
+    echo '<table class="search-table">
+        <tbody>
 
-if (isset($_POST['submit'])) {
+        <td>ID</td>
+        <td>PEALKIRI</td>
+        <td>AUTOR</td>
+        <td>ŽANR</td>
+        <td>KOGUS</td>
+
+        <tbody>
+        <table>';
+} else {
+
+    require "../config.php";
+    require "../connect.php";
+
     $keyword = $_POST['search'];
-    if($keyword==""){
-        echo "Sisestage otsitav.";
-    }else {
-        $found_books = mysqli_query($con, "
+    $found_books = mysqli_query($con, "
             SELECT books.book_id, books.book_title,books.book_quantiy,authors.author_name,genres.genre_name
             FROM books
             JOIN authors ON authors.author_id  = books.author_id
@@ -17,17 +29,31 @@ if (isset($_POST['submit'])) {
               genres.genre_id LIKE ('%$keyword%')
             ");
 
-        while ($book = MySQLi_fetch_array($found_books)) {
+    echo '<table class="search-table">
+        <tbody>
 
-            echo '<tr>';
-            echo '<td id="id">' . $book['book_id'] . '</td>';
-            echo '<td ><a href="?page=book&id=' . $book['book_id'] . '"> ' . $book['book_title'] . '</a></td>';
-            echo '<td id="author">' . $book['author_name'] . '</td>';
-            echo '<td id="type">' . $book['genre_name'] . '</td>';
-            echo '<td id="quantity">' . $book['book_quantiy'] . '</td>';
-            echo '</tr>';
+        <td>ID</td>
+        <td>PEALKIRI</td>
+        <td>AUTOR</td>
+        <td>ŽANR</td>
+        <td>KOGUS</td>
 
-        }
+        ';
+
+    while ($book = MySQLi_fetch_array($found_books)) {
+
+        echo '<tr>';
+        echo '<td id="id">' . $book['book_id'] . '</td>';
+        echo '<td ><a href="?page=book&id=' . $book['book_id'] . '"> ' . $book['book_title'] . '</a></td>';
+        echo '<td id="author">' . $book['author_name'] . '</td>';
+        echo '<td id="type">' . $book['genre_name'] . '</td>';
+        echo '<td id="quantity">' . $book['book_quantiy'] . '</td>';
+        echo '</tr>';
+
     }
+
+    echo '</tbody>
+        </table>';
 }
+
 
--- views/search_book.php
+++ views/search_book.php
@@ -1,25 +1,46 @@
 
+
 <h1><b>Raamatud</b></h1>
 
 <div class="search_book">
     <form method="post">
-        <input type="search" name="search" placeholder="Otsi raamatut..."/>
-        <input type="submit" name="submit" value="Otsi"/>
+        <input type="search" name="search" placeholder="Otsi raamatut..." id="search"/>
     </form>
     <br>
-    <table class="search-table">
-        <tbody>
 
-            <td>ID</td>
-            <td>PEALKIRI</td>
-            <td>AUTOR</td>
-            <td>ŽANR</td>
-            <td>KOGUS</td>
-
-            <?php require "controllers/search_book.php"; ?>
-
-        </tbody>
-    </table>
 </div>
 
 <br>
+
+<div id="result">
+    <table class="search-table">
+        <tbody>
+
+        <td>ID</td>
+        <td>PEALKIRI</td>
+        <td>AUTOR</td>
+        <td>ŽANR</td>
+        <td>KOGUS</td>
+
+        <tbody>
+        <table>
+</div>
+
+<script>
+
+    $("#search").keyup(function () {
+        var input = $("#search").val();
+        $.ajax({
+            type: 'POST',
+            url: 'controllers/search_book.php',
+            data: {
+                'search': input
+            },
+            success: function (data) {
+                console.log(data);
+                $("#result").html(data);
+            }
+        });
+    });
+
+</script>
